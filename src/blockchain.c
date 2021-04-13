f15cb5bedd63e2c897b2b7b1948a2a0aebb483fd
Adam Bruce <adambruce0108@gmail.com>
2021-04-13 17:36:05 +0000

    replaced INET_OFFSET

--- src/blockchain.c
+++ src/blockchain.c
@@ -21,7 +21,6 @@
 #include <arpa/inet.h>
 #endif
 
-#define INET_OFFSET 16
 #define PENDING_RULES_BUF_LEN 10
 static char pending_rules[PENDING_RULES_BUF_LEN][INET_ADDRSTRLEN];
 
@@ -36,20 +35,20 @@ int get_block_hash(unsigned char *buffer, FirewallBlock *block,
       return 1;
     }
 
-  memcpy(data_to_hash, block->author, INET_OFFSET);
-  memcpy(data_to_hash + INET_OFFSET, block->rule.source_addr,
+  memcpy(data_to_hash, block->author, INET_ADDRSTRLEN);
+  memcpy(data_to_hash + INET_ADDSTRLEN, block->rule.source_addr,
 	 INET_OFFSET);
-  memcpy(data_to_hash + INET_OFFSET * 2, block->rule.dest_addr,
+  memcpy(data_to_hash + INET_ADDRSTRLEN * 2, block->rule.dest_addr,
 	 INET_OFFSET);
-  memcpy(data_to_hash + INET_OFFSET * 3,
+  memcpy(data_to_hash + INET_ADDRSTRLEN * 3,
 	 (void*)&block->rule.source_port, 2);
-  memcpy(data_to_hash + (INET_OFFSET * 3) + 2,
+  memcpy(data_to_hash + (INET_ADDRSTRLEN * 3) + 2,
 	 (void*)&block->rule.dest_port, 2);
-  memcpy(data_to_hash + (INET_OFFSET * 3) + 4,
+  memcpy(data_to_hash + (INET_ADDRSTRLEN * 3) + 4,
 	 (void*)&block->rule.action, 4);
 
   SHA256_Init(&sha256);
-  SHA256_Update(&sha256, data_to_hash, (INET_OFFSET * 3) + 4 + 4);
+  SHA256_Update(&sha256, data_to_hash, (INET_ADDRSTRLEN * 3) + 4 + 4);
   SHA256_Final(buffer, &sha256);
  
   return 0;
